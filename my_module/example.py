c78746f001fa42bccbea83c490345f1c083ffcb5
Le4ebniyLem0n <Aminzapad@gmail.com>
2026-03-30 11:09:35 +0300

    Initial commit: add pipeline and example code

---
+++ my_module/example.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+def say_hello():
+    print("Hello, world!")
+
+def long_line():
+    # Это очень длинная строка, которая нарушает правило PEP 8 о максимальной длине строки в 79 символов
+    pass
+
+say_hello()
